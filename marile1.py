4711c4ef65dc60c365d9f891746dcf3404708a24
marile delgado <mariledelgado@hotmail.com>
2018-08-04 17:52:38 -0300

    Mi app Django Girls, primer commit0

---
+++ marile1.py
@@ -0,0 +1,18 @@
+def hi():
+	print('Hola!')
+	print('como estas?')
+hi()
+def hi(name):
+	if name == 'Marile':
+		print('Hola Marile')
+	elif name == 'Marile':
+		print('Hola Marile!')
+	else:
+		print('Hola desconocido!')
+def hi(name):
+	print('hi' + name + '!')
+chicas = ['Marile', 'Lolo', 'Kari', 'Pepa']
+for name in chicas:
+	hi(name)
+	print('sigiente chica')
+
